3ff4baafe0f19eba9d1fad576a4a0658ffa18d7a
somapon <sombororbund@gmail.com>
2016-08-25 17:04:54 -0700

    Change route file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('employee.employee-login.create');
 });
 
 Route::get('admin', function () {
@@ -21,6 +21,12 @@ Route::get('admin', function () {
 
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('admin/registration', 'Admin\\RegistrationController');
+	Route::resource('admin/department', 'Admin\\DepartmentController');
+	Route::resource('admin/designation', 'Admin\\DesignationController');
+	Route::resource('admin/employee', 'Admin\\EmployeeController');
+	Route::resource('admin/payment-status', 'Admin\\PaymentStatusController');
+	Route::resource('admin/salary-increment', 'Admin\\SalaryIncrementController');
+	Route::resource('admin/salary-payment', 'Admin\\SalaryPaymentController');
 	
 });
 //Route::resource('admin/user-logout', 'Admin\\UserLoginController');
@@ -29,13 +35,18 @@ Route::resource('admin/user-login', 'Admin\\UserLoginController');
 
 Route::get('admin/user-logout', ['uses' => 'Admin\\UserLoginController@logout', 'as' => 'logout']);
 
-Route::resource('admin/department', 'Admin\\DepartmentController');
-Route::resource('admin/designation', 'Admin\\DesignationController');
 
-Route::resource('admin/payment-status', 'Admin\\PaymentStatusController');
-Route::resource('admin/employee', 'Admin\\EmployeeController');
-Route::resource('admin/salary-increment', 'Admin\\SalaryIncrementController');
-Route::resource('admin/salary-payment', 'Admin\\SalaryPaymentController');
+
+
+
+
+
+
 Route::post('/salary-info', ['uses' => 'Admin\\SalaryPaymentController@postSalaryInfo', 'as' => 'salary-info']);
 Route::resource('admin/accounting', 'Admin\\AccountingController');
 Route::post('/account-info', ['uses' => 'Admin\\AccountingController@postAccountInfo', 'as' => 'account-info']);
+
+Route::resource('employee/employee-login', 'Employee\\EmployeeLoginController');
+Route::get('employee/salaryinfo/{emp_code}', ['uses' => 'Employee\\EmployeeLoginController@postSalaryInfo', 'as' => 'salaryinfo']);
+Route::get('employee/increment-info/{emp_code}', ['uses' => 'Employee\\EmployeeLoginController@postInrementInfo', 'as' => 'increment-info']);
+Route::get('employee/logout', ['uses' => 'Employee\\EmployeeLoginController@logout', 'as' => 'logout']);
