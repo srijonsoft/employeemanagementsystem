a47742c3b087922c2eb8427a00fc66588d3a34a9
somapon <sombororbund@gmail.com>
2016-08-28 00:24:26 -0700

    Edit js file

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,43 +1,86 @@
 
-	$(document).ready(function(){
-		$('#payment_status').change(function(){
+$(document).ready(function(){
+	$('#payment_status').change(function(){
+		
+		var status = $('#payment_status').val();
+		var salary = $('#salary').val();
+		
+		if(status == 2){
+			$('#due_amount').show();
+		}else if(status == 1){
+			$('#dueamount').val('');
+			$('#due_amount').hide();
 			
-			var status = $('#payment_status').val();
-			var salary = $('#salary').val();
-			
-			if(status == 2){
-				$('#due_amount').show();
-			}else if(status == 1){
-				$('#dueamount').val('');
-				$('#due_amount').hide();
-				
-			}
-			
-		});
+		}
+		
 	});
-	$(document).ready(function(){
-		$('#payment_amount').keyup(function(){
-			
-			var salary = $('#salary').val();
-			var payment_amount = $('#payment_amount').val();
-			var due = salary - payment_amount;
-			$('#dueamount').val(due);
-			
-		});
+});
+$(document).ready(function(){
+	$('#payment_amount').keyup(function(){
+		
+		var salary = $('#salary').val();
+		var payment_amount = $('#payment_amount').val();
+		var due = salary - payment_amount;
+		$('#dueamount').val(due);
+		
 	});
+});
 
 $(document).ready(function(){
-		$('#payment_status1').change(function(){
+	$('#payment_status1').change(function(){
+		
+		var status = $('#payment_status1').val();
+		
+		if(status == 1){
+			$('#due_amount1').hide();
 			
-			var status = $('#payment_status1').val();
+			var dueamount = $('#dueamount1').val();
 			
-			if(status == 1){
-				$('#due_amount1').hide();
-				
-				var dueamount = $('#dueamount1').val();
-				
-				$('#paymentamount1').val(dueamount);
-			
-			}
-			});
-		});
+			$('#paymentamount1').val(dueamount);
+		
+		}
+	});
+});
+
+$(document).ready(function(){
+    $('.filterable .btn-filter').click(function(){
+        var $panel = $(this).parents('.filterable'),
+        $filters = $panel.find('.filters input'),
+        $tbody = $panel.find('.table tbody');
+        if ($filters.prop('disabled') == true) {
+            $filters.prop('disabled', false);
+            $filters.first().focus();
+        } else {
+            $filters.val('').prop('disabled', true);
+            $tbody.find('.no-result').remove();
+            $tbody.find('tr').show();
+        }
+    });
+
+    $('.filterable .filters input').keyup(function(e){
+        /* Ignore tab key */
+        var code = e.keyCode || e.which;
+        if (code == '9') return;
+        /* Useful DOM data and selectors */
+        var $input = $(this),
+        inputContent = $input.val().toLowerCase(),
+        $panel = $input.parents('.filterable'),
+        column = $panel.find('.filters th').index($input.parents('th')),
+        $table = $panel.find('.table'),
+        $rows = $table.find('tbody tr');
+        /* Dirtiest filter function ever ;) */
+        var $filteredRows = $rows.filter(function(){
+            var value = $(this).find('td').eq(column).text().toLowerCase();
+            return value.indexOf(inputContent) === -1;
+        });
+        /* Clean previous no-result if exist */
+        $table.find('tbody .no-result').remove();
+        /* Show all rows, hide filtered ones (never do that outside of a demo ! xD) */
+        $rows.show();
+        $filteredRows.hide();
+        /* Prepend no-result row if all rows are filtered */
+        if ($filteredRows.length === $rows.length) {
+            $table.find('tbody').prepend($('<tr class="no-result text-center"><td colspan="'+ $table.find('.filters th').length +'">No result found</td></tr>'));
+        }
+    });
+});
