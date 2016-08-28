17362ca0dcc38a81f302daaef56e69326334188d
somapon <sombororbund@gmail.com>
2016-08-28 12:51:56 -0700

    Edit routes.php file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -27,26 +27,34 @@ Route::group(['middleware' => ['auth']], function () {
 	Route::resource('admin/payment-status', 'Admin\\PaymentStatusController');
 	Route::resource('admin/salary-increment', 'Admin\\SalaryIncrementController');
 	Route::resource('admin/salary-payment', 'Admin\\SalaryPaymentController');
+	Route::resource('admin/accounting', 'Admin\\AccountingController');
+	Route::resource('admin/mail-box', 'Admin\\MailBoxController');
 	
 });
-//Route::resource('admin/user-logout', 'Admin\\UserLoginController');
-Route::resource('admin/user-login', 'Admin\\UserLoginController');
 
+Route::post('/salary-info', ['uses' => 'Admin\\SalaryPaymentController@postSalaryInfo', 'as' => 'salary-info']);
+
+Route::post('/account-info', ['uses' => 'Admin\\AccountingController@postAccountInfo', 'as' => 'account-info']);
+
+Route::get('/userimage/{filename}', ['uses' => 'Admin\\RegistrationController@getUserImage', 'as' => 'account.image']);
+
+Route::resource('admin/user-login', 'Admin\\UserLoginController');
 
 Route::get('admin/user-logout', ['uses' => 'Admin\\UserLoginController@logout', 'as' => 'logout']);
 
 
+Route::post('admin/delete-mail', ['uses' => 'Admin\\MailBoxController@deleteMails', 'as' => 'delete-mail']);
 
 
+Route::group(['middleware' => ['employee']], function () {
+	Route::get('employee/salaryinfo/{emp_code}', ['uses' => 'Employee\\EmployeeLoginController@postSalaryInfo', 'as' => 'salaryinfo']);
+	Route::get('employee/increment-info/{emp_code}', ['uses' => 'Employee\\EmployeeLoginController@postInrementInfo', 'as' => 'increment-info']);
 
+});
 
-
-
-Route::post('/salary-info', ['uses' => 'Admin\\SalaryPaymentController@postSalaryInfo', 'as' => 'salary-info']);
-Route::resource('admin/accounting', 'Admin\\AccountingController');
-Route::post('/account-info', ['uses' => 'Admin\\AccountingController@postAccountInfo', 'as' => 'account-info']);
 
 Route::resource('employee/employee-login', 'Employee\\EmployeeLoginController');
-Route::get('employee/salaryinfo/{emp_code}', ['uses' => 'Employee\\EmployeeLoginController@postSalaryInfo', 'as' => 'salaryinfo']);
-Route::get('employee/increment-info/{emp_code}', ['uses' => 'Employee\\EmployeeLoginController@postInrementInfo', 'as' => 'increment-info']);
+
 Route::get('employee/logout', ['uses' => 'Employee\\EmployeeLoginController@logout', 'as' => 'logout']);
+
+Route::resource('employee/leave-application', 'Employee\\LeaveApplicationController');
